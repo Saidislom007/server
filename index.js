b2ad26f6b1bfbe2caf0d859e006b13997e2c1629
Saidislom <saidislomabdugafforov7@gmail.com>
2026-02-10 13:10:21 +0500

    Update CORS origin to new production URL

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://client-production-1ce9.up.railway.app", "http://localhost:5173"],
+    origin: ["https://www.alphadev.uz", "http://localhost:5173"],
     credentials: true,
   })
 );
