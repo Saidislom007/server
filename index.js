328e696519a8e243488896c63e20ff12cf0db1f9
Saidislom <saidislomabdugafforov7@gmail.com>
2026-03-24 10:42:43 +0500

    Add new origin to CORS configuration

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://www.alphadev.uz", "http://localhost:5173"],
+    origin: ["https://www.alphadev.uz", "http://localhost:5173","https://client-95yu.onrender.com",],
     credentials: true,
   })
 );
